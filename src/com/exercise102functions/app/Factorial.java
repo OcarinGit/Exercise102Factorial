781be4999f524f5e7288cb0b07cea1a55416d542
OcarinGit <oscarsantanaalvarez@gmail.com>
2019-10-16 15:24:23 -0500

    Factorial example in Java

---
+++ src/com/exercise102functions/app/Factorial.java
@@ -0,0 +1,41 @@
+package com.exercise102functions.app;
+
+import java.util.Scanner;
+public class Factorial {
+
+	public static void main(String[] args) 
+	{
+		//Variables declaration
+		long factorial = 1;
+		int number=-1;
+		
+		//Object construction
+		Scanner input = new Scanner(System.in);
+		
+		do
+		{
+			System.out.println("Number to calculate factorial:");
+			number=input.nextInt();
+			if(number<=0)
+			{
+				System.out.println("You must input a positive value");
+			}
+		}while (number<=0);
+		
+		factorial = GiveFactorial(number);
+		System.out.println("The \"factorial\" of "+ number+" is: \\"+factorial+", \n which is gotten by multiplying several times "+number);
+		System.out.println(String.format("The factorial of %d is: %d, which is gotten by multiplying several times %d ", number, factorial, number));
+		
+	}
+	
+	public static long GiveFactorial(int number)
+	{
+		int factorial=1;
+		for(int i=1;i<=number;i++)
+		{
+			factorial=factorial*i;
+		}
+		return factorial;
+	}
+	
+}
